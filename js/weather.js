e0469dc3410c498f9bedf061cf0cab3b2a6dab2e
qsTVT <17663460111@163.com>
2023-12-27 21:23:29 +0800

    Site updated: 2023-12-27 21:23:28

---
+++ js/weather.js
@@ -0,0 +1,24 @@
+WIDGET = {
+    "CONFIG": {
+      "modules": "01234",
+      "background": "5",
+      "tmpColor": "000000",
+      "tmpSize": "16",
+      "cityColor": "000000",
+      "citySize": "16",
+      "aqiColor": "000000",
+      "aqiSize": "16",
+      "weatherIconSize": "24",
+      "alertIconSize": "18",
+      "padding": "10px 10px 10px 10px",
+      "shadow": "1",
+      "language": "auto",
+      "borderRadius": "5",
+      "fixed": "true",
+      "vertical": "center",
+      "horizontal": "center",
+      "left": "200",
+      "top": "10",
+      "key": "fcd7e117bdb14cbe9e70ccee14d72079"
+    }
+  }
